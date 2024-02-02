e98cb1cef2a997526078dcf77c399d9a96ccdd4e
MikhailD <proxdme@mail.ru>
2024-02-02 14:26:38 +0300

    main and service added

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,15 @@
+public class CreditPaymentService {
+    public int calculate(int loanAmount, int numberPayments, double interestRate) {
+        double annuityRate;
+        int monthPays;
+        double intRateMonth;
+        intRateMonth = (interestRate /100) / numberPayments;
+        double a = (1 + intRateMonth);
+        double e = numberPayments;
+        double result = Math.pow(a,e);
+        annuityRate = (intRateMonth * result) / (result - 1);
+        monthPays = (int) (loanAmount * annuityRate);
+        return monthPays;
+    }
+}
+
